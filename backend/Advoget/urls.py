1b22b91621a0b20571b5c0928eba20b456ab07c2
Gustavo Oliveira de Souza <gustavoo.desouza@outlook.com>
2022-11-10 21:43:27 -0300

    Merge dev main (#1)
    Merge and squash dev main

--- backend/Advoget/urls.py
+++ backend/Advoget/urls.py
@@ -2,13 +2,11 @@ from django.conf.urls import include
 from django.urls import path
 from django.contrib import admin
 
-
 import django_js_reverse.views
 from rest_framework.routers import DefaultRouter
 from common.routes import routes as common_routes
 
 router = DefaultRouter()
-
 routes = common_routes
 for route in routes:
     router.register(route['regex'], route['viewset'], basename=route['basename'])
